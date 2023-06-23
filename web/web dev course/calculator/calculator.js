0727fa954374b326f206fa4d978ad9feb8183172
Mayank <innovativecreations195@gmail.com>
2023-06-24 04:00:53 +0530

    make calculator

--- web/web dev course/calculator/calculator.js
+++ web/web dev course/calculator/calculator.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
 });
 app.post("/", function(res, req){
-    console.log(res);
-    req.send("You clicked the calculate button");
+    var n1 = Number(res.body.num1);
+    var n2 = Number(res.body.num2);
+    result = n1 + n2;
+    console.log(res.body.num1);
+    req.send("You clicked the calculate button and your ans is " + result);
 });
 app.listen(3000);
