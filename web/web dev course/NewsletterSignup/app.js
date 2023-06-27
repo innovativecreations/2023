b4137b791b889f25e2e9612d9c81ecee30117ebc
Mayank <innovativecreations195@gmail.com>
2023-06-28 01:55:19 +0530

    complete ui

--- web/web dev course/NewsletterSignup/app.js
+++ web/web dev course/NewsletterSignup/app.js
@@ -3,19 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public/"))
+
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.get("/", function(req, res){
-    if(pass){
-        res.send("Registed successfully");
-    }
-    else{
-        res.send("SOme issue occured");
-    }
+app.post("/", function(req, res){
+    
+    var fName = req.body.fName;
+    var lName = req.body.lName;
+    var email = req.body.email;
+     console.log(fName, lName, email);
+    // if(pass){
+    //     res.send("Registed successfully");
+    // }
+    // else{
+    //     res.send("SOme issue occured");
+    // }
+    res.send("YO! I got you");
 });
 
 app.listen("3000");
